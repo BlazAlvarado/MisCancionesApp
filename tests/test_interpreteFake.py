df12547531e50c2761eb3d7f5c60b21a3d23369d
BlazAlvarado <71500070@continental.edu.pe>
2021-11-21 13:32:50 -0500

    CODIGO AGREGADO

--- tests/test_interpreteFake.py
+++ tests/test_interpreteFake.py
@@ -5,8 +5,6 @@ import random
 from faker import Faker
 
 from src.logica.coleccion import Coleccion
-from src.modelo.album import Album, Medio
-from src.modelo.cancion import Cancion, AlbumCancion
 from src.modelo.interprete import Interprete
 from src.modelo.declarative_base import Session
 from fake_providers import InterpreteNombreProvider, InterpreteTextoCuriosidadesProvider
@@ -28,14 +26,14 @@ class InterpreteTestCaseFake(unittest.TestCase):
             self.data.append(
                 (
                     self.data_factory.unique.interpreteNombre(),
-                    self.data_factory.interpreteTexto_curiosidades(),
+                    self.data_factory.interpreteTexto (),
                 )
             )
             self.interprete.append(
-                Album(
+                Interprete(
                     nombre=self.data[-1][0],
                     texto_curiosidades=self.data[-1][1],
-                    canciones=[]
+                    cancion=[]
                 )
             )
             self.session.add(self.interprete[-1])
@@ -45,8 +43,8 @@ class InterpreteTestCaseFake(unittest.TestCase):
             En este setUp no se cierra la sesión para usar
             los albumes en las pruebas
         '''
-        self.session.commit()
-        # self.session.close()
+        self.session.commit( )
+        #self.session.close()
 
     def tearDown(self):
         self.session = Session()
@@ -59,15 +57,15 @@ class InterpreteTestCaseFake(unittest.TestCase):
         self.session.close()
 
     def test_constructor(self):
-        for album, dato in zip(self.interprete, self.data):
-            self.assertEqual(album.nombre, dato[0])
-            self.assertEqual(album.texto_curiosidades, dato[1])
+        for interpretes, dato in zip(self.interprete, self.data):
+            self.assertEqual(interpretes.nombre, dato[0])
+            self.assertEqual(interpretes.texto_curiosidades, dato[1])
 
     def test_agregar_interpete(self):
         interpreteNombre = self.data_factory.unique.interpreteNombre()
-        interpreteTexto_curiosidades = self.data_factory.interpreteTexto_curiosidades()
+        interpreteTexto = self.data_factory.interpreteTexto()
 
-        resultado = self.logica.agregar_interprete(interpreteNombre, interpreteTexto_curiosidades)
+        resultado = self.logica.agregar_interprete(interpreteNombre, interpreteTexto)
 
         self.assertEqual(resultado, True)
 
@@ -75,7 +73,7 @@ class InterpreteTestCaseFake(unittest.TestCase):
         self.data.append(
             (
                 self.data_factory.unique.interpreteNombre(),
-                self.data_factory.interpreteTexto_curiosidades(),
+                self.data_factory.interpreteTexto(),
 
             )
         )
